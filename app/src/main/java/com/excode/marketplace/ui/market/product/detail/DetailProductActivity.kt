34690b7686c174d27668aa0df596a0cd8376ada4
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-10-04 11:17:02 +0700

    Slicing detail product

--- app/src/main/java/com/excode/marketplace/ui/market/product/detail/DetailProductActivity.kt
+++ app/src/main/java/com/excode/marketplace/ui/market/product/detail/DetailProductActivity.kt
@@ -3,18 +3,18 @@ package com.excode.marketplace.ui.market.product.detail
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.excode.marketplace.databinding.ActivityProductBinding
+import com.excode.marketplace.databinding.ActivityDetailProductBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DetailProductActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityProductBinding
+    private lateinit var binding: ActivityDetailProductBinding
     private val viewModel: DetailProductViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityProductBinding.inflate(layoutInflater)
+        binding = ActivityDetailProductBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
